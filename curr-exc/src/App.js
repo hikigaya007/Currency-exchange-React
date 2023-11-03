ad443cc466f38769206dfc970a6b2cfbb0d83d18
hikigaya007 <bijop007@gmail.com>
2023-11-04 00:47:00 +0530

    did some styling on background

--- curr-exc/src/App.js
+++ curr-exc/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <div className='inner-container' >
+            <div className="textbox1-container">
+                <p>From</p>
+                <input type="text" className="textbox1" />
+            </div>
+            <div className="textbox2-container">
+                <p>To</p>
+                <input type="text" className="textbox2" />
+            </div>
+        </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
