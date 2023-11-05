849ef36566024c864ef9ad00e6cd4317e8573c50
hikigaya007 <bijop007@gmail.com>
2023-11-05 20:06:22 +0530

    final UI build done

--- curr-exc/src/App.js
+++ curr-exc/src/App.js
@@ -2,18 +2,24 @@ import React from 'react'
 import './App.css';
 // https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr.json
 const App = () => {
-  
+
+  // amount,
+  // onAmount,
+  // onCurrencyChange,
+  // currencyOptions=[],
+  // selectCurrency = "usd"
+
   return (
     <div className='container'>
         <div className='inner-container' >
             <div className="textbox1-container">
                 <div className="textbox1-container1">
                   <p style={{marginLeft:'10px'}}>From</p>
-                  <input type="text" className="textbox1" />
+                  <input type="text" className="textbox1" placeholder='Amount'/>
                 </div>
                 <div className="textbox1-container2">
                   Currency Type:
-                  <select>
+                  <select style={{marginTop:'-20px'}}>
                     <option value="2">hello</option>
                     <option value="2"></option>
                     <option value="2"></option>
@@ -23,11 +29,11 @@ const App = () => {
             <div className="textbox1-container">
                 <div className="textbox1-container1">
                   <p style={{marginLeft:'10px'}}>To</p>
-                  <input type="text" className="textbox1" />
+                  <input type="text" className="textbox2" />
                 </div>
                 <div className="textbox1-container2">
                   Currency Type:
-                  <select>
+                  <select style={{marginTop:'-20px'}}>
                     <option value="USD">USD</option>
                     <option value="2"></option>
                     <option value="2"></option>
