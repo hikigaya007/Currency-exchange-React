c4999b368ead3ce243877cc8f0e707adae649c45
hikigaya007 <bijop007@gmail.com>
2023-11-05 19:49:39 +0530

    UI completed

--- curr-exc/src/App.js
+++ curr-exc/src/App.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import './App.css';
+// https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr.json
 const App = () => {
+  
   return (
     <div className='container'>
         <div className='inner-container' >
             <div className="textbox1-container">
-                <p style={{marginLeft:'10px'}}>From</p>
-                <input type="text" className="textbox1" />
+                <div className="textbox1-container1">
+                  <p style={{marginLeft:'10px'}}>From</p>
+                  <input type="text" className="textbox1" />
+                </div>
+                <div className="textbox1-container2">
+                  Currency Type:
+                  <select>
+                    <option value="2">hello</option>
+                    <option value="2"></option>
+                    <option value="2"></option>
+                  </select>
+                </div>
             </div>
-            <div className="textbox2-container">
-                <p style={{marginLeft:'10px'}}>To</p>
-                <input type="text" className="textbox2" />
+            <div className="textbox1-container">
+                <div className="textbox1-container1">
+                  <p style={{marginLeft:'10px'}}>To</p>
+                  <input type="text" className="textbox1" />
+                </div>
+                <div className="textbox1-container2">
+                  Currency Type:
+                  <select>
+                    <option value="USD">USD</option>
+                    <option value="2"></option>
+                    <option value="2"></option>
+                  </select>
+                </div>
             </div>
-            <input type="button" value={"Covert"} className='convert-button' />
+            <input type="button" value={"CONVERT"} className='convert-button' />
         </div>
     </div>
   )
