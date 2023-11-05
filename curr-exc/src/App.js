b255240f323cf2fa184cc529bf03c7bfc3e829c3
hikigaya007 <bijop007@gmail.com>
2023-11-05 23:36:43 +0530

    functionality work done

--- curr-exc/src/App.js
+++ curr-exc/src/App.js
@@ -1,46 +1,103 @@
-import React from 'react'
-import './App.css';
-// https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/inr.json
+import React, { useEffect, useState, useId} from 'react';
+import './App.css'
 const App = () => {
+  const [data, setData] = useState({});
+  const [amount, setAmount] = useState();
+  const [selectedCurrency, setSelectedCurrency] = useState('usd');
+  const [convertCurrency, setConvertCurrency] = useState('inr');
+  const [convertedAmount , setConvertedAmount] = useState();
+  async function useCurrencyInfo(currency) {
+    useEffect(() => {
+      async function fetchData() {
+        try {
+          const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`);
+          const responseData = await response.json();
+          setData(responseData[currency]);
+        } catch (error) {
+          console.error(error);
+        }
+      }
+  
+      fetchData();
+    }, [currency]);
+  }
+  
 
-  // amount,
-  // onAmount,
-  // onCurrencyChange,
-  // currencyOptions=[],
-  // selectCurrency = "usd"
+  const convert = () => {
+    const exchangeRate = data[convertCurrency] ;
+    console.log(data[convertCurrency]);
+    setConvertedAmount(amount * exchangeRate);
+  }
+
+  useCurrencyInfo(selectedCurrency);
+
+  const currencyOptions = Object.keys(data);
+
+  const amountInputId = useId();
+
+  const onAmountChange = (newAmount) => {
+    setAmount(newAmount);
+  };
+
+  const onCurrencyChange = (event) => {
+    setSelectedCurrency(event.target.value);
+  };
+
+  const onOutputCurrency = (e)=> {
+    setConvertCurrency(e.target.value);
+    console.log(e.target.value);
+  };
 
   return (
     <div className='container'>
+      <p className='title'>Currency Exchange</p>
         <div className='inner-container' >
             <div className="textbox1-container">
                 <div className="textbox1-container1">
                   <p style={{marginLeft:'10px'}}>From</p>
-                  <input type="text" className="textbox1" placeholder='Amount'/>
+                  <input 
+                  id={amountInputId}
+                  type="text" 
+                  className="textbox1" 
+                  placeholder='Amount' 
+                  value={amount}
+                  onChange={(e) => onAmountChange && onAmountChange((e.target.value))}/>
                 </div>
                 <div className="textbox1-container2">
                   Currency Type:
-                  <select style={{marginTop:'-20px'}}>
-                    <option value="2">hello</option>
-                    <option value="2"></option>
-                    <option value="2"></option>
+                  <select  style={{marginTop:'-20px'}} onChange={(e) => onCurrencyChange(e)}>
+                    {currencyOptions.map((currency) => (
+                      <option key={currency} value={currency}>{currency.toUpperCase()}
+                      </option>
+                    ))}
+                    
                   </select>
                 </div>
             </div>
             <div className="textbox1-container">
                 <div className="textbox1-container1">
                   <p style={{marginLeft:'10px'}}>To</p>
-                  <input type="text" className="textbox2" />
+                  <input 
+                  type="text" 
+                  className="textbox2" 
+                  readOnly={true}
+                  value={convertedAmount}/>
                 </div>
                 <div className="textbox1-container2">
                   Currency Type:
-                  <select style={{marginTop:'-20px'}}>
-                    <option value="USD">USD</option>
-                    <option value="2"></option>
-                    <option value="2"></option>
+                  <select style={{marginTop:'-20px'}} onChange={(e) => onOutputCurrency(e)} >
+                    {currencyOptions.map((currency) => (
+                      <option key={currency} value={currency}>{currency.toUpperCase()}</option>
+                    ))}
                   </select>
                 </div>
             </div>
-            <input type="button" value={"CONVERT"} className='convert-button' />
+            <input 
+            type="button" 
+            value={"CONVERT"} 
+            className='convert-button' 
+            onClick={convert}
+            />
         </div>
     </div>
   )
